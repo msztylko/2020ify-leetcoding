e077e23d5860fb8dbb01e3816795a94fbc29f747
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-09-17 09:51:06 +0200

    test for problem 0136

---
+++ tests/test_0136.py
@@ -0,0 +1,16 @@
+import ctypes
+import pathlib
+import os
+
+SOLUTIONS_PATH = '/home/marcin/code/2020ify-leetcoding/solutions/0136-single-number'
+
+c_path = os.path.join(SOLUTIONS_PATH, 'single-number.so')
+c_lib  = ctypes.CDLL(c_path)
+c_lib.singleNumber.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int]
+
+def test_single_number():
+    array = [4, 1, 2, 1, 2]
+    # Transform Python array to C arr
+    arr = (ctypes.c_int * len(array))(*array)
+    out = c_lib.singleNumber(arr, len(arr))
+    assert out == 4

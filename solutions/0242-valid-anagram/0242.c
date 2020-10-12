908f034c71d0e26b9e8299af64c0fe17b3536219
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-10-12 10:21:57 +0200

    solution to problem 0242

---
+++ solutions/0242-valid-anagram/0242.c
@@ -0,0 +1,22 @@
+bool isAnagram(char *s, char *t)
+{
+    int s_count[26] = {0}, t_count[26] = {0};
+    int i, slen = strlen(s), tlen = strlen(t);
+    
+    for (i = 0; i < slen; i++) {
+        int c = s[i] - 'a';
+        s_count[c] += 1;
+    }
+    
+    for (i = 0; i < tlen; i++) {
+        int c = t[i] - 'a';
+        t_count[c] += 1;
+    }
+    
+    for (i = 0; i < 26; i++) {
+        if (s_count[i] != t_count[i])
+            return false;
+    }
+    
+    return true;
+}

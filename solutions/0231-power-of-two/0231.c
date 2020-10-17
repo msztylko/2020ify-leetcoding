0cb7eb91cafe6cd1bf61bc3372fd5fedcc028dbb
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-10-17 11:31:26 +0200

    bit manipulation solution to p0231

--- solutions/0231-power-of-two/0231.c
+++ solutions/0231-power-of-two/0231.c
@@ -1,6 +1,6 @@
 #include <stdbool.h>
 
-bool isPowerOfTwo(int n){
+bool isPowerOfTwoNaive(int n){
     if (n < 1) return false;
     
     while(n != 1) {
@@ -9,4 +9,13 @@ bool isPowerOfTwo(int n){
         n /= 2;
     }
     return true;
+}
+
+bool isPowerOfTwoBit(int n) {
+    if (n < 1) return false;
+    
+    if ((n & (n - 1)) == 0)
+        return true;
+    else
+        return false;
 }

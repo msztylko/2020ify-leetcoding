7a6d77f60e9a66eb09e25b44dfc7ebe17f9a6b17
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-10-17 11:00:43 +0200

    small improvement to p0326

--- solutions/0326-power-of-three/0326.c
+++ solutions/0326-power-of-three/0326.c
@@ -1,13 +1,13 @@
 #include <stdbool.h>
 
-bool isPowerOfThree(int n){
-    if (n == 0) return false;
+bool isPowerOfThree(int n) {
+    if (n < 1) return false;
     
     while(n != 1) {
         if (n % 3 != 0){
             return false;
         }
-        n = n / 3;
+        n /= 3;
     }
     return true;
 }

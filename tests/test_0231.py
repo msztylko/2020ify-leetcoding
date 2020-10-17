b99c36c79bee39901ee2855b254586372c59363d
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-10-17 11:31:44 +0200

    update tests for p0231

--- tests/test_0231.py
+++ tests/test_0231.py
@@ -2,9 +2,15 @@ import ctypes
 
 c_path = '/home/marcin/code/2020ify-leetcoding/solutions/0231-power-of-two/power-of-two.so'
 c_lib = ctypes.CDLL(c_path)
-c_lib.isPowerOfTwo.argtypes = [ctypes.c_int]
+c_lib.isPowerOfTwoNaive.argtypes = [ctypes.c_int]
+c_lib.isPowerOfTwoBit.argtypes = [ctypes.c_int]
 
-def test_power_of_two():
+def test_power_of_two_naive():
     number = 16
-    out = c_lib.isPowerOfTwo(number)
+    out = c_lib.isPowerOfTwoNaive(number)
+    assert out
+
+def test_power_of_two_bit():
+    number = 16
+    out = c_lib.isPowerOfTwoBit(number)
     assert out

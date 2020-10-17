accd8201c49f70c2554d28145a6bf1bb384ab0a4
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-10-17 11:42:52 +0200

    solution to problem 0342

---
+++ solutions/0342-power-of-four/0342.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+
+bool isPowerOfFour(int num){
+    if (num < 1) return false;
+    
+    while (num != 1) {
+        if (num % 4 != 0)
+            return false;
+        num /= 4;
+    }
+    return true;
+}

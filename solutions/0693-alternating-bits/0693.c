96570a1f688879d847b9def4ae34e5d23ede64ec
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-10-20 12:10:59 +0200

    solution to p0693 + update Makefile

---
+++ solutions/0693-alternating-bits/0693.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+
+bool hasAlternatingBits(int n){
+    int last = n % 2;
+    int rest = n / 2;
+    
+    while (last <= rest) {
+        if (last == (rest % 2))
+            return false;
+        else {
+            last = rest % 2;
+            rest /= 2;
+        }
+    }
+    return true;
+}

2939f3220fc94b024efb5145587d38b2274a8aec
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-10-20 09:44:00 +0200

    solution to p0476 + update Makefile

---
+++ solutions/0476-number-complement/0476.c
@@ -0,0 +1,12 @@
+int findComplement(int N) {
+    if(N == 0) return 1;
+    
+    long ret = N;
+    long mask = 1;
+    
+    while(mask <= ret) {   
+        ret = ret ^ mask;  
+        mask = mask << 1;  
+    }
+    return ret;
+}

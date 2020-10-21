637a097148578129c677901b4ddaca50f8daaa6e
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-10-21 09:59:59 +0200

    remove redundant declaration

--- tests/test_0136.py
+++ tests/test_0136.py
@@ -2,7 +2,6 @@ import ctypes
 
 c_path = '/home/marcin/code/2020ify-leetcoding/solutions/0136-single-number/single-number.so'
 c_lib  = ctypes.CDLL(c_path)
-c_lib.singleNumber.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int]
 
 def test_single_number():
     array = [4, 1, 2, 1, 2]

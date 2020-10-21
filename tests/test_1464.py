2e98ef7ad606fbb228d774af0069d12f07193bc1
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-10-21 09:39:57 +0200

    test for p1464

---
+++ tests/test_1464.py
@@ -0,0 +1,11 @@
+import ctypes
+
+c_path = '/home/marcin/code/2020ify-leetcoding/solutions/1464-max-product/max-product.so'
+c_lib = ctypes.CDLL(c_path)
+c_lib.maxProduct.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int]
+
+def test_max_product():
+    array = [3,4,5,2]
+    arr = (ctypes.c_int * len(array))(*array)
+    out = c_lib.maxProduct(arr, len(arr))
+    assert out == 12

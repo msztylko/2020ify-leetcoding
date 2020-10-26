7a7c0e9e3dec0164c6a2baa169704b701c207dbf
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-10-26 06:58:11 +0100

    add Tree struct definition

--- solutions/0965-univalued-binary-tree/0965.c
+++ solutions/0965-univalued-binary-tree/0965.c
@@ -1,11 +1,13 @@
-/**
- *  * Definition for a binary tree node.
- *    struct TreeNode {
- *          int val;
- *          struct TreeNode *left;
- *          struct TreeNode *right;
- *    };
- */
+#include <stdbool.h>
+
+
+/* Definition for a binary tree node. */
+struct TreeNode {
+      int val;
+      struct TreeNode *left;
+      struct TreeNode *right;
+};
+
 
 bool isUnivalTree(struct TreeNode* root) {
         

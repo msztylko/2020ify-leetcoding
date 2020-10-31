d6a0cb45524e6d1b914a8a4fddc222221473c5e7
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-10-31 14:52:05 +0100

    test for p1523

---
+++ tests/test_1523.py
@@ -0,0 +1,10 @@
+import ctypes
+import pytest
+
+c_lib = ctypes.CDLL('../solutions/1523-count-odd/count-odd.so')
+
+@pytest.mark.parametrize("function", [c_lib.countOddsNaive, c_lib.countOddsFast])
+def test_count_odds(function):
+    low, high = 3, 7
+    out = function(low, high)
+    assert out == 3

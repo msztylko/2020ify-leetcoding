1180f6d0048996061e591e1e04005e3f09042121
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-01 10:43:36 +0100

    solution to p0387 + update Makefile

---
+++ solutions/0387-unique-character/0387.c
@@ -0,0 +1,13 @@
+int firstUniqChar(char * s) {
+    int h[26] = {0};
+    
+    for(int i = 0; s[i] != '\0'; i++) {
+        h[s[i] - 'a']++;
+    }
+    
+    for(int i = 0; s[i] != '\0'; i++) {
+        if (h[s[i] - 'a'] == 1) 
+            return i; 
+    }
+    return -1;
+}

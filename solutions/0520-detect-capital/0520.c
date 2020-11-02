3db66323c14ab924b712f4f0fe7b85afbd829257
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-02 14:03:09 +0100

    description for p0520

---
+++ solutions/0520-detect-capital/0520.c
@@ -0,0 +1,23 @@
+#include <stdbool.h>
+
+bool detectCapitalUse(char * word) {
+    if (strlen(word) == 1) { return true; }
+    
+    int i, small = 0, big = 0;
+    
+    for(i = 0; word[i]; i++) {
+        if (word[i]>='a' && word[i] <='z')
+            small++;
+        if (word[i]>='A' && word[i] <='Z')
+            big++;
+    }
+    
+    if (small == 0 && big != 0)
+        return true;
+    else if (big == 0 && small != 0)
+        return true;
+    else if (big == 1 && word[0]>='A' && word[0] <='Z')
+        return true;
+    else 
+        return false;
+}

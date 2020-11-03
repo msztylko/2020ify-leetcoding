62f279bf8bcae0b2af8ebacaf82dfb773241776d
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-03 09:12:45 +0100

    solution for p0551 + update Makefile

---
+++ solutions/0551-student-attendance/0551.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdbool.h>
+
+bool checkRecord(char * s) {
+    int Acounter = 0;
+    int Lcounter = 0;
+    int length = strlen(s);
+    
+    for (int i = 0; i < length; i++) {
+        if (s[i] == 'A')
+            Acounter++;
+        if (s[i] == 'L') {
+            if (i + 2 < length && s[i+1] == 'L' && s[i+2] == 'L')
+                return false;
+        }
+    }
+    return (Acounter > 1) ? false : true;
+}

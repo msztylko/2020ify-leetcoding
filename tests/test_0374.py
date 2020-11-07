9a972af50b96c612151cfdec87a99b4d81ac2cea
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-07 15:33:50 +0100

    test placeholder for p0374 + update Makefile

---
+++ tests/test_0374.py
@@ -0,0 +1,8 @@
+import ctypes
+import pytest
+
+c_lib = ('../solutions/0374-guess-number/guess-number.so')
+
+@pytest.mark.skip(reason="Implement problem's API first")
+def test_guess_number():
+    pass

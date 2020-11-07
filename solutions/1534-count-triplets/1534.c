7e8e93fb81d624ccea81c28b8e34b54f8d6d0699
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-07 14:27:11 +0100

    p1534 - faster inner loop

--- solutions/1534-count-triplets/1534.c
+++ solutions/1534-count-triplets/1534.c
@@ -6,10 +6,12 @@ int countGoodTriplets(int* arr, int arrSize, int a, int b, int c) {
     int counter = 0;
     for (int i = 0; i < arrSize; i++) {
         for (int j  = i + 1; j < arrSize; j++) {
+            if (absDiff(arr[i], arr[j]) > a) {
+                continue;
+            }
             for (int k = j + 1; k < arrSize; k++) {
-                if(absDiff(arr[i], arr[j]) <= a &&
-                   absDiff(arr[j], arr[k]) <= b &&
-                   absDiff(arr[i], arr[k]) <= c ){
+                if(absDiff(arr[j], arr[k]) <= b &&
+                   absDiff(arr[i], arr[k]) <= c ) {
                     counter++;
                 }
             }

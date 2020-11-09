c3347b0a59ffca6c19b1ed2179c3f2d41089e0fe
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-09 10:02:35 +0100

    extra solution for p1512

--- solutions/1512-good-pair/1512.c
+++ solutions/1512-good-pair/1512.c
@@ -1,4 +1,5 @@
-int numIdenticalPairs(int* nums, int numsSize){
+// Space efficient implementation O(1) memory
+int numIdenticalPairsSpace(int* nums, int numsSize){
     int counter = 0;
     for (int i = 0; i < numsSize; i++) {
         for (int j = i + 1; j < numsSize; j++) {
@@ -6,6 +7,20 @@ int numIdenticalPairs(int* nums, int numsSize){
                 counter++;
             }
         }
+    }
+    return counter;
+}
+
+// Time efficient implementation O(n) time
+int numIdenticalPairsTime(int* nums, int numsSize){
+    int hashTable[101] = { 0 };
+    int counter = 0;
+    
+    for(int i = 0; i < numsSize; i++) {
+        hashTable[nums[i]]++;
+        if (hashTable[nums[i]] > 1) {
+            counter += hashTable[nums[i]]-1;
+        } 
     }
     return counter;
 }

dc26cd9f84915e15fae06131f5324b94ae0e5894
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-10 20:06:32 +0100

    faster solution for p0027 + modify test

--- solutions/0027-remove-element/0027.c
+++ solutions/0027-remove-element/0027.c
@@ -8,3 +8,17 @@ int removeElement(int* nums, int numsSize, int val) {
     }
     return i;
 }
+
+int removeElementFaster(int* nums, int numsSize, int val) {
+    int i = 0;
+    
+    while (i < numsSize) {
+        if (nums[i] == val) {
+            nums[i] = nums[numsSize - 1];
+            numsSize--;
+        } else {
+            i++;
+        }
+    }
+    return numsSize;
+}
--- tests/test_0027.py
+++ tests/test_0027.py
@@ -1,10 +1,13 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0027-remove-element/remove-element.so')
 
-def test_remove_element():
+@pytest.mark.parametrize('function', [c_lib.removeElement,
+                                      c_lib.removeElementFaster])
+def test_remove_element(function):
     array = [3,2,2,3]
     target = 3
     arr = (ctypes.c_int * len(array))(*array)
-    out = c_lib.removeElement(arr, len(arr), target)
+    out = function(arr, len(arr), target)
     assert out == 2

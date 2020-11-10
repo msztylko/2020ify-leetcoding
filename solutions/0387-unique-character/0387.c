d6ec59dfb29ff384076d5e691d48ef874d9085e6
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-10 09:26:48 +0100

    simplify p0387

--- solutions/0387-unique-character/0387.c
+++ solutions/0387-unique-character/0387.c
@@ -1,11 +1,11 @@
 int firstUniqChar(char * s) {
     int h[26] = {0};
     
-    for(int i = 0; s[i] != '\0'; i++) {
+    for(int i = 0; s[i]; i++) {
         h[s[i] - 'a']++;
     }
     
-    for(int i = 0; s[i] != '\0'; i++) {
+    for(int i = 0; s[i]; i++) {
         if (h[s[i] - 'a'] == 1) 
             return i; 
     }

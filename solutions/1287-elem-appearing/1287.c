4f0f39ad09c48657718ecd0956783ee1019b33c1
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-10 09:41:50 +0100

    faster solution for p1287

--- solutions/1287-elem-appearing/1287.c
+++ solutions/1287-elem-appearing/1287.c
@@ -1,18 +1,17 @@
 int findSpecialInteger(int* arr, int arrSize) {
-    if (arrSize < 2)
-        return *arr;
+    if (arrSize < 2) return *arr;
     
-    int percent = arrSize / 4;
-    int counter;
+    int threshold = arrSize / 4;
+    int counter = 1;
     
-    for (int i = 0; i < arrSize; i++) {
-        counter = 1;
-        for (int j = i + 1; j < arrSize; j++) {
-            if (arr[i] == arr[j])
-                counter++;
-        }     
-        if (counter > percent) {
-            return arr[i];
+    for (int i = 0; i < arrSize - 1; i++) {
+        if (arr[i] == arr[i+1]) {
+            counter++;
+            if (counter > threshold) {
+                return arr[i];
+            }
+        } else {
+            counter = 1;
         }
     }
     return 0;

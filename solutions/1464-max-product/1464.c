0677cbedf5b68115ed75302c931d68ac5dfc3a15
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-12 08:44:55 +0100

    faster solution for p1464 + update tests

--- solutions/1464-max-product/1464.c
+++ solutions/1464-max-product/1464.c
@@ -1,13 +1,29 @@
-int maxProduct(int *nums, int numsSize){
+int maxProductNaive(int *nums, int numsSize) {
     long max = 0, product = 0;
     int i, j;
     
-    for (i = 0; i < numsSize - 1; i++) {
+    for (i = 0; i < numsSize; i++) {
         for (j = i + 1; j < numsSize; j++) {
             product = (nums[i]-1) * (nums[j]-1);
             if (product > max)
                 max = product;
         }
     }
+    return max;
+}
+
+int maxProductOnePass(int *nums, int numsSize) {
+    int max = 0;
+    int first = 0, second = 0;
+    
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] > first) {
+            second = first;
+            first = nums[i];
+        } else if (nums[i] > second) {
+            second = nums[i];
+        }
+    }
+    max = (first-1) * (second-1); 
     return max;
 }
--- tests/test_1464.py
+++ tests/test_1464.py
@@ -1,9 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1464-max-product/max-product.so')
 
-def test_max_product():
+@pytest.mark.parametrize('function', [c_lib.maxProductNaive,
+                                      c_lib.maxProductOnePass])
+def test_max_product(function):
     array = [3,4,5,2]
     arr = (ctypes.c_int * len(array))(*array)
-    out = c_lib.maxProduct(arr, len(arr))
+    out = function(arr, len(arr))
     assert out == 12

fbcde1f2e94cdd824daaa4887cbdf3060b32bf49
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-13 09:21:58 +0100

    simplify solution for p0724

--- solutions/0724-pivot-index/0724.c
+++ solutions/0724-pivot-index/0724.c
@@ -1,25 +1,17 @@
-int leftSum(int index, int* arr) {
-    int sum = 0;
-    for (int i = 0; i < index; i++)
-        sum += arr[i];
-    return sum;
-}
-
-int rightSum(int index, int size, int*arr) {
-    int sum = 0;
-    for (int i = index + 1; i <= (size - 1); i++)
-        sum += arr[i];
-    return sum;
-}
-
-int pivotIndex(int* nums, int numsSize){
-
+int pivotIndex(int* nums, int numsSize) {
+    if (numsSize == 0) return -1;
+    
+    int sum = 0, leftSum = 0, rightSum;
+    
     for (int i = 0; i < numsSize; i++) {
-        int lSum = leftSum(i, nums);
-        int rSum = rightSum(i, numsSize, nums);
-                
-        if (lSum == rSum)
+        sum  += nums[i];
+    }
+    
+    for (int i = 0; i < numsSize; i++) {
+        rightSum = sum - leftSum - nums[i];
+        if(leftSum == rightSum)
             return i;
+        leftSum += nums[i];
     }
     return -1;
 }

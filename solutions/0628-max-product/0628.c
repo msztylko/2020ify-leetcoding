75fc7779ad9f60ded4cace8ab9aef034be7110e7
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-14 11:14:27 +0100

    simplify solution for p0628

--- solutions/0628-max-product/0628.c
+++ solutions/0628-max-product/0628.c
@@ -1,29 +1,26 @@
-#include <limits.h>
+#define MAX(a,b) (a>b) ? a : b
 
-int max(int num1, int num2) {
-    return (num1 > num2 ) ? num1 : num2;
-}
-
-int min(int num1, int num2) {
-    return (num1 > num2 ) ? num2 : num1;
-}
-
-int maximumProduct(int *nums, int numsSize) {
-    int f[2][4], i, j;
-    f[0][0] = f[1][0] = 1;
-    for (j = 3; j > 0; --j) {
-        f[0][j] = INT_MIN;
-        f[1][j] = INT_MAX;
-    }        
-        
-    for (i = 0; i < numsSize; ++i) {
-        for (j = 3; j > 0; --j) {
-            if (f[0][j - 1] == INT_MIN) {
-                continue;
-            }
-            f[0][j] = max(f[0][j], max(f[0][j - 1] * nums[i], f[1][j - 1] * nums[i]));
-            f[1][j] = min(f[1][j], min(f[0][j - 1] * nums[i], f[1][j - 1] * nums[i]));
+int maximumProduct(int* nums, int numsSize) {
+    int min1 = 1001, min2 = 1001;
+    int max1 = -1001, max2 = -1001, max3 = -1001;
+    
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] < min1) {
+            min2 = min1;
+            min1 = nums[i];
+        } else if (nums[i] < min2) {
+            min2 = nums[i];
         }
-    }                              
-    return f[0][3];
+        if (nums[i] > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = nums[i];
+        } else if (nums[i] > max2) {
+            max3 = max2;
+            max2 = nums[i];
+        } else if (nums[i] > max3) {
+            max3 = nums[i];
+        }
+    }
+    return MAX(min1 * min2 * max1, max1 * max2 * max3);
 }

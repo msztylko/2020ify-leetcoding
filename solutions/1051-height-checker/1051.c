3f2d7f62e40f58621548b4c16ec25aca1250882a
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-14 12:02:55 +0100

    simplify solution for p1051

--- solutions/1051-height-checker/1051.c
+++ solutions/1051-height-checker/1051.c
@@ -3,14 +3,13 @@
 int heightChecker(int* heights, int heightsSize) {    
     int *myArray = malloc(sizeof(int) * heightsSize);
     int counter = 0;
-    int min;
     
     for (int i = 0; i < heightsSize; i++) {
         myArray[i] = heights[i];
     }
     
     for (int i = 0; i < heightsSize; i++) {
-        min = i;
+        int min = i;
         for (int j = i + 1; j < heightsSize; j++) {
             if (myArray[j] < myArray[min]) {
                 min = j;

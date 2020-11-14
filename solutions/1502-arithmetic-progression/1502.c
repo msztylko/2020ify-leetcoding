3f727a582c69c70617518a3d052873b8f1bd8418
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-14 12:33:54 +0100

    faster solution for p1502

--- solutions/1502-arithmetic-progression/1502.c
+++ solutions/1502-arithmetic-progression/1502.c
@@ -1,34 +1,16 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int absDiff(long int a, long int b) {
-        return (a > b) ? a-b : b-a;
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
 }
 
 bool canMakeArithmeticProgression(int* arr, int arrSize) {
-    int *myArray = malloc(sizeof(int) * arrSize);
-    int d;
-
-    for (int i = 0; i < arrSize; i++) {
-        myArray[i] = arr[i];
-    }
+    qsort(arr, arrSize, sizeof(int), cmpfunc);
+    int d = arr[1] - arr[0];
     
-    for (int i = 0; i < arrSize; i++) {
-        int min = i;
-        for (int j = i + 1; j < arrSize; j++) {
-            if (myArray[j] < myArray[min]) {
-                min = j;
-            }
-        }
-        int temp = myArray[i];
-        myArray[i] = myArray[min];
-        myArray[min] = temp;
-    }
-    d = absDiff(myArray[0], myArray[1]);
-    for (int i = 0; i < arrSize - 1; i++) {
-        if (absDiff(myArray[i], myArray[i+1]) != d) {
+    for(int i = 2; i < arrSize; i++)
+        if(arr[i] - arr[i-1] != d) 
             return false;
-        }
-    }
     return true;
 }

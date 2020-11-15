e333fa4c947c7f9db9f6d3cf2237faa55e42c1ed
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-15 07:34:20 +0100

    simplify solution for p1207

--- solutions/1207-unique-number/1207.c
+++ solutions/1207-unique-number/1207.c
@@ -1,7 +1,7 @@
 #include <stdbool.h>
 
 bool uniqueOccurrences(int* arr, int arrSize) {
-    int values[2001] = { 0 };
+    int values[2001] = {0};
     int occurrencies[2001] = {0};
     int i;
     
@@ -10,10 +10,8 @@ bool uniqueOccurrences(int* arr, int arrSize) {
     }
     
     for (i = 0; i < 2001; i++) {
-        if(values[i] != 0) {
-            if(++occurrencies[values[i]] > 1)
+        if (values[i] && ++occurrencies[values[i]] > 1)
                 return false;
-        }
     }
     return true;
 }

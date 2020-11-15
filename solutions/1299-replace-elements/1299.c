d1e16b0e66f34315761920cd2e4e7ef8a2f99801
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-15 07:49:21 +0100

    faster solution for p1299 + update test

--- solutions/1299-replace-elements/1299.c
+++ solutions/1299-replace-elements/1299.c
@@ -1,3 +1,5 @@
+#define MAX(a,b) (a>b) ? a : b
+
 int* replaceElements(int* arr, int arrSize, int* returnSize){
     int max, i, j;
     
@@ -10,6 +12,18 @@ int* replaceElements(int* arr, int arrSize, int* returnSize){
         arr[i] = max;
     }
     arr[i] = -1;
+    *returnSize = arrSize;
+    return arr;
+}
+
+int* replaceElementsOptimized(int* arr, int arrSize, int* returnSize) {
+    int max = -1;
+    
+    for (int i = arrSize - 1; i >= 0; i--) {
+        int tmp = arr[i];
+        arr[i] = max;
+        max = MAX(tmp, max);
+    }
     *returnSize = arrSize;
     return arr;
 }
--- tests/test_1299.py
+++ tests/test_1299.py
@@ -4,8 +4,10 @@ import pytest
 c_lib = ctypes.CDLL('../solutions/1299-replace-elements/replace-elements.so')
 
 @pytest.mark.skip(reason="Segmentation fault")
-def test_replace_elements():
+@pytest.mark.parametrize('function', [c_lib.replaceElements,
+                                      c_lib.replaceElementsOptimized])
+def test_replace_elements(function):
     array = [17,18,5,4,6,1]
     arr = (ctypes.c_int * len(array))(*array)
-    out = c_lib.replaceElements(arr, len(arr), len(arr))
+    out = function(arr, len(arr), len(arr))
     assert arr == [18,6,6,6,1,-1]

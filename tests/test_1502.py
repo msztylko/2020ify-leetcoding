5003f81a796ace059c2b81c60cdfeb8e8274b1c1
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-19 20:55:48 +0100

    parametrize test for p1502

--- tests/test_1502.py
+++ tests/test_1502.py
@@ -1,9 +1,13 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1502-arithmetic-progression/arithmetic-progression.so')
 
-def test_arithmetic_progression():
-    array = [-509,-19,-439,-264,-404,-369,-299,-89,-229,-54,-194,16,-544,-159,-124,-474,-334]
+@pytest.mark.parametrize('array, ans', 
+[([-509,-19,-439,-264,-404,-369,-299,-89,-229,-54,-194,16,-544,-159,-124,-474,-334], True),
+ ([3,5,1], True),
+ ([1,2,4], False)])
+def test_arithmetic_progression(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.canMakeArithmeticProgression(arr, len(arr))
-    assert out
+    assert out == ans

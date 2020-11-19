488d3689111d12d93674bee2b8fe38284be5b3b0
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-19 20:53:27 +0100

    parametrize test for p1346

--- tests/test_1346.py
+++ tests/test_1346.py
@@ -1,9 +1,14 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1346-check-exist/check-exist.so')
 
-def test_check_exist():
-    array = [7,1,14,11]
+@pytest.mark.parametrize('array, ans', 
+                        [([10,2,5,3], True),
+                         ([7,1,14,11], True),
+                         ([3,1,7,11], False)])
+                        
+def test_check_exist(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.checkIfExist(arr, len(arr))
-    assert out
+    assert out == ans

09694464fe20073af3f0be59edb7468ec7bbefcc
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-19 21:01:41 +0100

    parametrize test for p0201

--- tests/test_0201.py
+++ tests/test_0201.py
@@ -3,8 +3,10 @@ import pytest
 
 c_lib = ctypes.CDLL('../solutions/0201-bitwise-and/bitwise-and.so')
 
-@pytest.mark.parametrize("function", [c_lib.rangeBitwiseAndNaive, c_lib.rangeBitwiseAndFast])
-def test_bitwise_and(function):
-    a, b = 0, 214748364
+@pytest.mark.parametrize('function', [c_lib.rangeBitwiseAndNaive, c_lib.rangeBitwiseAndFast])
+@pytest.mark.parametrize('a, b, ans', 
+                        [(0, 1, 0),
+                         (0, 214748364, 0)])
+def test_bitwise_and(a, b, ans, function):
     out = function(a, b)
-    assert out == 0
+    assert out == ans

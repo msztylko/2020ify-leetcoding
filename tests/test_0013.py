cba1a252ed0e5f67bf7e45ecfa3b660d1ca62abf
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-20 16:37:01 +0100

    parametrize test for p0013

--- tests/test_0013.py
+++ tests/test_0013.py
@@ -1,8 +1,14 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0013-roman-to-integer/roman-to-integer.so')
 
-def test_roman_to_integer():
-    roman = 'V'
+@pytest.mark.parametrize('roman, ans', 
+                        [(b'III', 3),
+                         (b'IV', 4),
+                         (b'IX', 9),
+                         (b'LVIII', 58),
+                         (b'MCMXCIV', 1994)])
+def test_roman_to_integer(roman, ans):
     out = c_lib.romanToInt(roman)
-    assert out == 5
+    assert out == ans

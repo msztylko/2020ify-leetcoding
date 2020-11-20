fbfde24546caa4a62d1bd99760c2b633d84a53e7
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-20 16:29:42 +0100

    parametrize test for p0007

--- tests/test_0007.py
+++ tests/test_0007.py
@@ -1,8 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0007-reverse-integer/reverse-integer.so')
 
-def test_reverse_integer():
-    x = 123
+@pytest.mark.parametrize('x, ans', [(123, 321),
+                                    (-123, -321),
+                                    (120, 21),
+                                    (0, 0)])
+def test_reverse_integer(x, ans):
     out = c_lib.reverse(x)
-    assert out == 321
+    assert out == ans

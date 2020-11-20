2da5551d1ff9aed60b8166f6a527b0853cf477a0
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-20 16:40:07 +0100

    parametrize test for p0035

--- tests/test_0035.py
+++ tests/test_0035.py
@@ -1,10 +1,15 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0035-search-insert/search-insert.so')
 
-def test_search_insert():
-    array = [1,3,5,6]
-    target = 2
+@pytest.mark.parametrize('array, target, ans',
+                        [([1,3,5,6], 5, 2),
+                         ([1,3,5,6], 2, 1),
+                         ([1,3,5,6], 7, 4),
+                         ([1,3,5,6], 0, 0),
+                         ([1], 0, 0)])
+def test_search_insert(array, target, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.searchInsert(arr, len(arr), target)
-    assert out == 1
+    assert out == ans

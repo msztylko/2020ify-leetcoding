bb41440bdd0834333aedd760087a91374af81870
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-21 16:36:33 +0100

    parametrize test for p0191

--- tests/test_0191.py
+++ tests/test_0191.py
@@ -1,8 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0191-number-1-bits/number-1-bits.so')
 
-def test_number_of_1_bits():
-    number = 0b00000000000000000000000000001011
-    out =  c_lib.hammingWeight(number)
-    assert out == 3
+@pytest.mark.parametrize('num, ans',
+[(0b00000000000000000000000000001011, 3),
+ (0b00000000000000000000000010000000, 1),
+ (0b11111111111111111111111111111101, 31)])
+def test_number_of_1_bits(num, ans):
+    out =  c_lib.hammingWeight(num)
+    assert out == ans

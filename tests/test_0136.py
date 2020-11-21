e78d8fd8645b0615cf846dec90fc847d0c912b0e
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-21 16:34:14 +0100

    parametrize test for p0136

--- tests/test_0136.py
+++ tests/test_0136.py
@@ -1,10 +1,14 @@
 import ctypes
+import pytest
 
 c_lib  = ctypes.CDLL('../solutions/0136-single-number/single-number.so')
 
-def test_single_number():
-    array = [4, 1, 2, 1, 2]
+@pytest.mark.parametrize('array, ans',
+                        [([2,2,1], 1),
+                         ([4,1,2,1,2], 4),
+                         ([1], 1)])
+def test_single_number(array, ans):
     # Transform Python array to C arr
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.singleNumber(arr, len(arr))
-    assert out == 4
+    assert out == ans

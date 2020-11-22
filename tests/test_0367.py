d491702f15bd1629ec4b70dfb1a4ff97a9d227db
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-22 17:37:50 +0100

    parametrize test for p0367

--- tests/test_0367.py
+++ tests/test_0367.py
@@ -1,8 +1,11 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0367-valid-perfect-square/valid-perfect-square.so')
 
-def test_valid_perfect_square():
-    number = 256
-    out = c_lib.isPerfectSquare(number)
-    assert out
+@pytest.mark.parametrize('num, ans', [(0, False),
+                                      (16, True),
+                                      (14, False)])
+def test_valid_perfect_square(num, ans):
+    out = c_lib.isPerfectSquare(num)
+    assert out == ans

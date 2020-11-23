cb7044d64dfdc98f98ab5f69b5adec23ebf7742b
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-23 09:48:43 +0100

    parametrize test for p0268

--- tests/test_0268.py
+++ tests/test_0268.py
@@ -1,9 +1,14 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0268-missing-number/missing-number.so')
 
-def test_missing_number():
-    array = [9,6,4,2,3,5,7,0,1]
+@pytest.mark.parametrize('array, ans', 
+                        [([3,0,1], 2),
+                         ([0,1], 2),
+                         ([9,6,4,2,3,5,7,0,1], 8),
+                         ([0], 1)])
+def test_missing_number(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.missingNumber(arr, len(arr))
-    assert out == 8
+    assert out == ans

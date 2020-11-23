5ce869acf42edf4a62c7365f4c002e16d807a8e9
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-23 09:46:53 +0100

    parametrize test for p0263

--- tests/test_0263.py
+++ tests/test_0263.py
@@ -1,8 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0263-ugly-number/ugly-number.so')
 
-def test_ugly_number():
-    number = 512
-    out =  c_lib.isUgly(number)
-    assert out
+@pytest.mark.parametrize('num, ans', [(6, True),
+                                      (512, True),
+                                      (14, False),
+                                      (1, True)])
+def test_ugly_number(num, ans):
+    out =  c_lib.isUgly(num)
+    assert out == ans

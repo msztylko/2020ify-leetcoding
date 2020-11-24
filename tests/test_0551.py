7774903eeb149fafe9bbd12372df9b2e1a5890a0
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-24 18:27:31 +0100

    parametrize test for p0551

--- tests/test_0551.py
+++ tests/test_0551.py
@@ -1,8 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0551-student-attendance/student-attendance.so')
 
-def test_student_attendance():
-    array = b"PPPLPPLLLLPPPLLPPPLPLLAPLPPLPL"
+@pytest.mark.parametrize('array, ans',
+                        [(b"PPALLP", True),
+                         (b"PPALLL", False),
+                         (b"PPPLPPLLLLPPPLLPPPLPLLAPLPPLPL", False)])
+def test_student_attendance(array, ans):
     out = c_lib.checkRecord(array)
-    assert not out
+    assert out == ans

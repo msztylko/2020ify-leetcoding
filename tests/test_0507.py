2387fcad5f79c4f5b8da9e8f6f5bcbad69b19f68
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-24 18:23:26 +0100

    parametrize test for p0507

--- tests/test_0507.py
+++ tests/test_0507.py
@@ -1,8 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0507-perfect-number/perfect-number.so')
 
-def test_perfect_number():
-    number = 28
-    out = c_lib.checkPerfectNumber(number)
-    assert out
+@pytest.mark.parametrize('num, ans',
+                        [(28, True),
+                         (6, True),
+                         (2, False)])
+def test_perfect_number(num, ans):
+    out = c_lib.checkPerfectNumber(num)
+    assert out == ans

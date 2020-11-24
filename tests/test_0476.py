3ecb9cb207cc780086484b6b77fa4704f84ee974
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-24 18:20:13 +0100

    parametrize test for p0476

--- tests/test_0476.py
+++ tests/test_0476.py
@@ -3,7 +3,7 @@ import pytest
 
 c_lib = ctypes.CDLL('../solutions/0476-number-complement/number-complement.so')
 
-@pytest.mark.parametrize('num, ans', [(5,2), (1,0)])
+@pytest.mark.parametrize('num, ans', [(5,2), (1,0), (257, 254)])
 def test_complement_number(num, ans):
     out = c_lib.findComplement(num)
     assert out == ans

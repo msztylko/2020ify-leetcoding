eed006548677b04ea4c1b7cd7821ad11ed768cbd
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-25 21:27:10 +0100

    parametrize test for p0896

--- tests/test_0896.py
+++ tests/test_0896.py
@@ -1,9 +1,15 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0896-monotonic-array/monotonic-array.so')
 
-def test_monotonic_array():
-    array = [6,5,4,4]
+@pytest.mark.parametrize('array, ans',
+[([1,2,2,3], True),
+ ([6,5,4,4], True),
+ ([1,3,2], False),
+ ([1,2,4,5], True),
+ ([1,1,1], True)])
+def test_monotonic_array(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.isMonotonic(arr, len(arr))
-    assert out
+    assert out == ans

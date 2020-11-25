fc3ac644e515faaf55d198b364dafa47b9521b0a
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-25 21:35:03 +0100

    parametrize test for p0747

--- tests/test_0747.py
+++ tests/test_0747.py
@@ -1,9 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0747-dominant-index/dominant-index.so')
 
-def test_dominat_index():
-    array = [0,0,1,2]
+@pytest.mark.parametrize('array, ans',
+                        [([3, 6, 1, 0], 1),
+                         ([1, 2, 3, 4], -1)])
+def test_dominat_index(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.dominantIndex(arr, len(arr))
-    assert out == 3
+    assert out == ans

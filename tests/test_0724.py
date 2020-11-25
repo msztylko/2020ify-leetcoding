ff0eb1334620918835bde12bdd7f3c566b28bd40
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-25 21:36:53 +0100

    parametrize test for p0724

--- tests/test_0724.py
+++ tests/test_0724.py
@@ -1,9 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0724-pivot-index/pivot-index.so')
 
-def test_pivot_index():
-    array = [-1,-1,-1,0,1,1]
+@pytest.mark.parametrize('array, ans',
+                        [([1,7,3,6,5,6], 3),
+                         ([1,2,3], -1)])
+def test_pivot_index(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.pivotIndex(arr, len(arr))
-    assert out == 0
+    assert out == ans

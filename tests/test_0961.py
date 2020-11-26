1efd18da8fd01c599ee235be57ce12c496a26af1
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-26 18:44:05 +0100

    parametrize test for p0961

--- tests/test_0961.py
+++ tests/test_0961.py
@@ -1,9 +1,13 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0961-repeated-element/repeated-element.so')
 
-def test_repeated_element():
-    array = [1, 2, 3, 3]
+@pytest.mark.parametrize('array, ans',
+                        [([1,2,3,3], 3),
+                         ([2,1,2,5,3,2], 2),
+                         ([5,1,5,2,5,3,5,4], 5)])
+def test_repeated_element(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.repeatedNTimes(arr, len(arr))
-    assert out == 3 
+    assert out == ans 

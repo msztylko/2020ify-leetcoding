81cdf82a9b6c06f4da04bf826b3092a7e4ab38e0
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-26 18:39:31 +0100

    parametrize test for p1207

--- tests/test_1207.py
+++ tests/test_1207.py
@@ -1,9 +1,13 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1207-unique-number/unique-number.so')
 
-def test_unique_number():
-    array = [1,2,2,1,1,3]
+@pytest.mark.parametrize('array, ans',
+                        [([1,2,2,1,1,3], True),
+                         ([1,2], False),
+                         ([-3,0,1,-3,1,1,1,-3,10,0], True)])
+def test_unique_number(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.uniqueOccurrences(arr, len(arr))
-    assert out
+    assert out == ans

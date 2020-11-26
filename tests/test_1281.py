6f98c76ec04a7c1e21761b461d6f95a36eab4c70
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-26 18:37:23 +0100

    parametrize test for p1281

--- tests/test_1281.py
+++ tests/test_1281.py
@@ -1,8 +1,10 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1281-product-sum/product-sum.so')
 
-def test_product_sum():
-    number = 12345
-    out = c_lib.subtractProductAndSum(number)
-    assert out == 105
+@pytest.mark.parametrize('num, ans', [(234, 15),
+                                      (4421, 21)])
+def test_product_sum(num, ans):
+    out = c_lib.subtractProductAndSum(num)
+    assert out == ans

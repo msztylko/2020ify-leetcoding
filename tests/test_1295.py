7eae9d203c99e29c8edb5e772a422847ac6e2d13
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-26 18:35:47 +0100

    parametrize test for p1295

--- tests/test_1295.py
+++ tests/test_1295.py
@@ -1,9 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1295-even-digits/even-digits.so')
 
-def test_even_digits():
-    array = [12,345,2,6,7896]
+@pytest.mark.parametrize('array, ans',
+                        [([12,345,2,6,7896], 2),
+                         ([555,901,482,1771], 1)])
+def test_even_digits(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.findNumbers(arr, len(arr))
-    assert out == 2
+    assert out == ans

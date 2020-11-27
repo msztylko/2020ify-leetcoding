e12b878cf4f0fc3579d55fde34b97dc294f3d1b5
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-27 09:52:22 +0100

    parametrize test for p1323

--- tests/test_1323.py
+++ tests/test_1323.py
@@ -1,8 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1323-maximum-69-number/maximum-69-number.so')
 
-def test_maximum_69_number():
-    number = 9669
-    out = c_lib.maximum69Number(number)
-    assert out == 9969
+@pytest.mark.parametrize('num, ans',
+                        [(9669, 9969),
+                         (9996, 9999),
+                         (9999, 9999)])
+def test_maximum_69_number(num, ans):
+    out = c_lib.maximum69Number(num)
+    assert out == ans

2a195b6ed6cc768c7f64334066a78354335fd1aa
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-27 09:32:32 +0100

    parametrize test for p1550

--- tests/test_1550.py
+++ tests/test_1550.py
@@ -1,9 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1550-consecutive-odd/consecutive-odd.so')
 
-def test_consecutive_odd():
-    array = [2,6,4,1]
+@pytest.mark.parametrize('array, ans',
+                        [([2,6,4,1], False),
+                         ([1,2,34,3,4,5,7,23,12], True)])
+def test_consecutive_odd(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.threeConsecutiveOdds(arr, len(arr))
-    assert out == False
+    assert out == ans

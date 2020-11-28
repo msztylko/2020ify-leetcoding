75c4d666ed17957b3fd1ef5353221fb8f7b2304e
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-28 22:15:57 +0100

    parametrize test for p1394

--- tests/test_1394.py
+++ tests/test_1394.py
@@ -1,9 +1,15 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1394-lucky-number/lucky-number.so')
 
-def test_lucky_number():
-    array = [7,7,7,7,7,7,7]
+@pytest.mark.parametrize('array, ans',
+[([2,2,3,4], 2),
+ ([1,2,2,3,3,3], 3),
+ ([2,2,2,3,3], -1),
+ ([5], -1),
+ ([7,7,7,7,7,7,7], 7)])
+def test_lucky_number(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.findLucky(arr, len(arr))
-    assert out == 7
+    assert out == ans

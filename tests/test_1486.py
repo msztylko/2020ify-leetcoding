86d59c991a9b14f3ebc2998c291a54241a66e5a7
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-28 22:17:59 +0100

    parametrize test for p1486

--- tests/test_1486.py
+++ tests/test_1486.py
@@ -1,8 +1,13 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1486-xor-array/xor-array.so')
 
-def test_xor_array():
-    n, start = 5, 0
+@pytest.mark.parametrize('n, start, ans',
+                        [(5, 0, 8),
+                         (4, 3, 8),
+                         (1, 7, 7),
+                         (10, 5, 2)])
+def test_xor_array(n, start, ans):
     out = c_lib.xorOperation(n, start)
-    assert out == 8
+    assert out == ans

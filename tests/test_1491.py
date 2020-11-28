bcd419333bc00e68f946ce640290eaedfaf2c9e1
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-28 22:20:14 +0100

    parematrize test for p1491

--- tests/test_1491.py
+++ tests/test_1491.py
@@ -1,10 +1,15 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1491-average-salary/average-salary.so')
 c_lib.average.restype = ctypes.c_double
 
-def test_average_salary():
-    array = [4000, 3000, 1000 ,2000]
+@pytest.mark.parametrize('array, ans',
+[([4000,3000,1000,2000], 2500.0),
+ ([1000,2000,3000], 2000.0),
+ ([6000,5000,4000,3000,2000,1000], 3500.0),
+ ([8000,9000,2000,3000,6000,1000], 4750.0)])
+def test_average_salary(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.average(arr, len(arr))
-    assert out == 2500.0
+    assert out == ans

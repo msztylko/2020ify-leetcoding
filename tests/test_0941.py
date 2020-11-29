3bda26971925fc854e729a6e30867c75a504ac1a
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-29 20:08:01 +0100

    parametrize test for p0941

--- tests/test_0941.py
+++ tests/test_0941.py
@@ -1,9 +1,13 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0941-mountain-array/mountain-array.so')
 
-def test_mountain_array():
-    array = [0,3,2,1]
+@pytest.mark.parametrize('array, ans',
+                        [([2,1], False),
+                         ([3,5,5], False),
+                         ([0,3,2,1], True)])
+def test_mountain_array(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.validMountainArray(arr, len(arr))
-    assert out
+    assert out == ans

990de4e5a81fe38a7e987a0e77f58493a14772f7
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-29 20:14:33 +0100

    parametrize test for p1534

--- tests/test_1534.py
+++ tests/test_1534.py
@@ -1,12 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1534-count-triplets/count-triplets.so')
 
-def test_count_triplets():
-    array = [3,0,1,1,9,7]
-    a = 7
-    b = 2
-    c = 3
+@pytest.mark.parametrize('array, a, b, c, ans',
+                [([3,0,1,1,9,7], 7, 2, 3, 4),
+                 ([1,1,2,2,3],   0, 0, 1, 0)])
+def test_count_triplets(array, a, b, c, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.countGoodTriplets(arr, len(arr), a, b, c)
-    assert out == 4
+    assert out == ans

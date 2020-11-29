eac8ea27691dfe4d21a9655a622a06d24b4770a5
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-11-29 20:09:55 +0100

    parametrize test for p1051

--- tests/test_1051.py
+++ tests/test_1051.py
@@ -1,9 +1,13 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1051-height-checker/height-checker.so')
 
-def test_height_checker():
-    array = [5,1,2,3,4]
+@pytest.mark.parametrize('array, ans',
+                        [([1,1,4,2,1,3], 3),
+                         ([5,1,2,3,4], 5),
+                         ([1,2,3,4,5], 0)])
+def test_height_checker(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.heightChecker(arr, len(arr))
-    assert out == 5
+    assert out == ans

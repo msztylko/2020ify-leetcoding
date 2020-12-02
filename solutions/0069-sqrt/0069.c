1ff056e02fec0cde75bb9935e0d54b98e5647c67
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-02 08:11:57 +0100

    simplify solution for p0069

--- solutions/0069-sqrt/0069.c
+++ solutions/0069-sqrt/0069.c
@@ -1,11 +1,9 @@
-int mySqrt(int x){
-    if (x == 0 || x == 1) return x;
-
-    long long int i = 1;
-    long long sqrt = 1;
+int mySqrt(int x) {
+    long i = 1, sqrt = 1;
+        
     while(sqrt <= x) {
         i++;
-        sqrt = i*i;
+        sqrt = i*i;   
     }
     return i -1;
 }

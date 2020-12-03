a6dee4c340b773be10aa335430ea67c6fab21baf
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-03 22:34:30 +0100

    parametrize test for p0709

--- tests/test_0709.py
+++ tests/test_0709.py
@@ -1,8 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0709-to-lower/to-lower.so')
 
-def test_to_lower():
-    string = b"Hello"
+@pytest.mark.parametrize('string, ans',
+                        [(b"Hello", b"hello"),
+                         (b"here", b"here"),
+                         (b"LOVELY", b"lovely")])
+def test_to_lower(string, ans):
     c_lib.toLowerCase(string)
-    assert string == b"hello"
+    assert string == ans

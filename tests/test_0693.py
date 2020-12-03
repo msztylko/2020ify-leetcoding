f8feda0271cfc312907d2a8d075e724fcbc8d6de
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-03 22:32:29 +0100

    parametrize test for p0693

--- tests/test_0693.py
+++ tests/test_0693.py
@@ -1,8 +1,13 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0693-alternating-bits/alternating-bits.so')
 
-def test_alternating_bits():
-    number = 3
-    out = c_lib.hasAlternatingBits(number)
-    assert out != True
+@pytest.mark.parametrize('num, ans', 
+                        [(5, True),
+                         (7, False),
+                         (11, False),
+                         (10, True)]) 
+def test_alternating_bits(num, ans):
+    out = c_lib.hasAlternatingBits(num)
+    assert out == ans

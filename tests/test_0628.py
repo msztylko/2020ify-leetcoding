c0187b0dd3ad6d178e8720c087513291d18f03ee
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-03 22:30:04 +0100

    parametrize test for p0628

--- tests/test_0628.py
+++ tests/test_0628.py
@@ -1,9 +1,13 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0628-max-product/max-product.so')
 
-def test_max_product():
-    array = [-1,-2,-3]
+@pytest.mark.parametrize('array, ans',
+                        [([1,2,3], 6),
+                         ([1,2,3,4], 24),
+                         ([-1,-2,-3], -6)])
+def test_max_product(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.maximumProduct(arr, len(arr))
-    assert out == -6
+    assert out == ans

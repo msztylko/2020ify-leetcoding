8077fbc77a66e7754b874f79b2c4e919d65e48ca
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-04 09:32:11 +0100

    parametrize test for p0026

--- tests/test_0026.py
+++ tests/test_0026.py
@@ -1,9 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0026-duplicate-sorted/duplicate-sorted.so')
 
-def test_duplicate_sorted():
-    array = [0,0,1,1,1,2,2,3,3,4]
+@pytest.mark.parametrize('array, ans',
+[([1,1,2], 2),
+ ([0,0,1,1,1,2,2,3,3,4], 5)])
+def test_duplicate_sorted(array, ans):
     arr = (ctypes.c_int * len(array))(*array)
     out = c_lib.removeDuplicates(arr, len(arr))
-    assert out == 5
+    assert out == ans

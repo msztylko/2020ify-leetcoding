67a176060cbe0bb1e99dbe53273db692f8e8bd25
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-05 20:12:51 +0100

    parametrize test for p0190

--- tests/test_0190.py
+++ tests/test_0190.py
@@ -1,8 +1,12 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0190-reverse-bits/reverse-bits.so')
 
-def test_reverse_bits():
-    number = 0b00000010100101000001111010011100
-    out = c_lib.reverseBits(number)
-    assert out == 964176192
+@pytest.mark.parametrize('num, ans',
+[(0b00000010100101000001111010011100, 964176192),
+ (0b11111111111111111111111111111101, 3221225471)])
+def test_reverse_bits(num, ans):
+    out = c_lib.reverseBits(num)
+    # "cast" to unsigned. Underlying representation stays the same
+    assert out & 0xffffffff == ans

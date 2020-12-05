ef6bdf5168332faa4bf4d98b6e599f78d04b9b7a
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-05 20:00:03 +0100

    parametrize test for p0371

--- tests/test_0371.py
+++ tests/test_0371.py
@@ -1,9 +1,11 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/0371-sum-two-integers/sum-two-integers.so')
 
-def test_sum_two_integers():
-    a = 2
-    b = -5
+@pytest.mark.parametrize('a, b, ans',
+                        [(1, 2, 3),
+                         (-2, 3, 1)])
+def test_sum_two_integers(a, b, ans):
     out = c_lib.getSum(a, b)
-    assert out == -3
+    assert out == ans

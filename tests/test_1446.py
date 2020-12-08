abadb429bfb69df4db453bb9e6c6c2b86b08c105
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-08 19:28:50 +0100

    parametrize test for p1446

--- tests/test_1446.py
+++ tests/test_1446.py
@@ -1,8 +1,14 @@
 import ctypes
+import pytest
 
 c_lib = ctypes.CDLL('../solutions/1446-consecutive-characters/consecutive-characters.so')
 
-def test_consecutive_characters():
-    string = b"leetcode"
+@pytest.mark.parametrize('string, ans',
+[(b"leetcode", 2),
+ (b"abbcccddddeeeeedcba", 5),
+ (b"triplepillooooow", 5),
+ (b"hooraaaaaaaaaaay", 11),
+ (b"tourist", 1)])
+def test_consecutive_characters(string, ans):
     out = c_lib.maxPower(string)
-    assert out == 2
+    assert out == ans

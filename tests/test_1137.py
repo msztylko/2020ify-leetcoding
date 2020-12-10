a231fa0a25519bf425cf1def07ca9360626995e1
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-10 19:52:40 +0100

    update tests for p1137

--- tests/test_1137.py
+++ tests/test_1137.py
@@ -7,6 +7,7 @@ c_lib = ctypes.CDLL('../solutions/1137-tribonacci/tribonacci.so')
                         [(4, 4),
                          (25, 1389537),
                          (37, 2082876103)])
-def test_tribonacci(num, ans):
-    out = c_lib.tribonacci(num)
+@pytest.mark.parametrize('function', [c_lib.tribonacci, c_lib.tribonacci_array])
+def test_tribonacci(num, ans, function):
+    out = function(num)
     assert out == ans

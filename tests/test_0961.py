7cf52544de77d6d022044087414fdc76296ed6d2
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-14 12:17:25 +0100

    update test for p0961

--- tests/test_0961.py
+++ tests/test_0961.py
@@ -7,7 +7,9 @@ c_lib = ctypes.CDLL('../solutions/0961-repeated-element/repeated-element.so')
                         [([1,2,3,3], 3),
                          ([2,1,2,5,3,2], 2),
                          ([5,1,5,2,5,3,5,4], 5)])
-def test_repeated_element(array, ans):
+@pytest.mark.parametrize('function', [c_lib.repeatedNTimes, 
+                                      c_lib.repeatedNTimesHashTable])
+def test_repeated_element(function, array, ans):
     arr = (ctypes.c_int * len(array))(*array)
-    out = c_lib.repeatedNTimes(arr, len(arr))
+    out = function(arr, len(arr))
     assert out == ans 

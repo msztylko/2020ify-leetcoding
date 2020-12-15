d55dc2a4c32812f28190df8dc29207a55a5ddeda
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-15 14:11:22 +0100

    simplify solution for p0278

--- solutions/0278-first-bad-version/0278.c
+++ solutions/0278-first-bad-version/0278.c
@@ -10,18 +10,17 @@ int firstBadVersionLinear(int n) {
     return -1;
 }
 
-int firstBadVersionBinSearch(int n) {
-    int low = 1;
-    int high = n;
-    int mid;
+int firstBadVersion(int n) {
+    int low = 1, high = n, mid;
     
     while (low <= high) {
         mid = low + (high - low) / 2;
         if (isBadVersion(mid)) {
             if (mid > 1 && !isBadVersion(mid - 1)) {
                 return mid;
+            } else {
+                high = mid - 1;    
             }
-            high = mid - 1;
         } else {
             low = mid + 1;
         }

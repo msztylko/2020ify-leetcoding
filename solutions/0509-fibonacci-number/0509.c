01c5443392f77ce8ae656bc7cad85b8ea1e3e17d
Marcin Sztylko <marcin.sztylko@gmail.com>
2021-01-08 08:19:44 +0100

    faster solution for p0509

--- solutions/0509-fibonacci-number/0509.c
+++ solutions/0509-fibonacci-number/0509.c
@@ -4,3 +4,16 @@ int fib(int N) {
     
     return fib(N-1) + fib(N-2);
 }
+
+int fibArray(int n) {
+    
+    int F[31] = {0};
+    F[0] = 0;
+    F[1] = 1;
+    
+    for (int i = 2; i <= n; i++) {
+        F[i] = F[i-2] +  F[i-1];
+    }
+    
+    return F[n];
+}
